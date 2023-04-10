714d6c643742b04e22b774873655cbf2558d36b5
Vitaliy <bezuglivita123@gmail.com>
2023-04-10 22:10:37 +0300

    deleted client port

--- Common/Contracts/ConnectionInformation.cs
+++ Common/Contracts/ConnectionInformation.cs
@@ -3,7 +3,6 @@ namespace Common.Contracts;
 public static class ConnectionInformation
 {
     public const int ServerPort = 5010;
-    public const int ClientPort = 5011;
     
     public const string ServerIp = "127.0.0.1";
 }
